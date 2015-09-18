59301b6f109a7dd3657e25054e8767036437d2b1
Corrie <corriesmith87@gmail.com>
2015-09-18 11:05:51 -0700

    rewill charge .50 for extra toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-function Pizza(pizzaSize) {
-  this.pizzaSize = pizzaSize;
+function Pizza(pizzaSize, extraToppings) {
+this.pizzaSize = pizzaSize;
+this.extraToppings = extraToppings;
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  if (this.pizzaSize === "small") {
+  if (this.pizzaSize === "small" && this.extraToppings === false) {
     return "$8.50";
-  } else if (this.pizzaSize === "medium") {
+  } else if (this.pizzaSize === "medium" && this.extraToppings === false) {
     return "$10.50";
-  } else if (this.pizzaSize === "large"){
+  } else if (this.pizzaSize === "large" && this.extraToppings === false){
     return "$12.50";
   }
 
+  if (this.extraToppings === true && this.pizzaSize === "small") {
+    return "$10.00";
+  } else if (this.extraToppings === true && this.pizzaSize === "medium") {
+    return "$12.00";
+  } else if (this.extraToppings === true && this.pizzaSize === "large") {
+    return "$14.00";
+  }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,11 @@
 describe("Pizza", function() {
   it("receives pizza size and returns price of pizza", function() {
-    var testPizza = new Pizza("small", true);
+    var testPizza = new Pizza("small", false);
     expect(testPizza.pizzaPrice()).to.equal("$8.50");
+  });
+
+  it("charges $1.50 for extra toppings", function() {
+    var testPizza = new Pizza("medium", true);
+    expect(testPizza.pizzaPrice()).to.equal("$12.00");
   });
 });
