f2d1d1d312c3a020f01d78959828dd7804d254ce
Corrie <corriesmith87@gmail.com>
2015-09-18 12:20:03 -0700

    add front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 function Pizza(pizzaSize, extraToppings) {
 this.pizzaSize = pizzaSize;
 this.extraToppings = extraToppings;
+// this.howMany = howMany;
 }
 
 Pizza.prototype.pizzaPrice = function() {
@@ -8,7 +9,7 @@ Pizza.prototype.pizzaPrice = function() {
     return "$8.50";
   } else if (this.pizzaSize === "medium" && this.extraToppings === false) {
     return "$10.50";
-  } else if (this.pizzaSize === "large" && this.extraToppings === false){
+  } else if (this.pizzaSize === "large" && this.extraToppings === false) {
     return "$12.50";
   }
 
@@ -19,4 +20,46 @@ Pizza.prototype.pizzaPrice = function() {
   } else if (this.extraToppings === true && this.pizzaSize === "large") {
     return "$14.00";
   }
+
+  // if (this.howMany === 2 && this.pizzaSize === "large" && this.extraToppings === false) {
+  //   return "$28.00";
+  // } else if (this.howMany === 2 && this.pizzaSize === "medium" && this.extraToppings === false) {
+  //   return "$24.00";
+  // } else if (this.howMany === 2 && this.pizzaSize === "small" && this.extraToppings === false) {
+  //   return "$20.00";
+  // }
 };
+
+function resetSelect() {
+  $("select#pizzaName").val("");
+  $("select#pizzaTopping").val("");
+  $("select#pizzaSize").val("");
+
+}
+
+$(document).ready(function() {
+  $("form#pizzaType").submit(function(event){
+      event.preventDefault();
+
+      var selectedPizza = $("select#pizzaName").val();
+      var selectedToppings = $("select#pizzaTopping").val();
+      var selectedSize = $("select#pizzaSize").val();
+      var extraToppings = false;
+
+      if (selectedToppings === "All of the above") {
+        extraToppings = true;
+      }
+
+      var newPizza = new Pizza(selectedSize, extraToppings);
+      var pizzaPrice = newPizza.pizzaPrice();
+
+      resetSelect();
+
+      $(".selectedSize").text(selectedSize);
+      $(".selectedPizza").text(selectedPizza);
+      $(".selectedToppings").text(selectedToppings);
+      $(".pizzaPrice").text(pizzaPrice);
+
+      $("#result").show();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,11 @@
 describe("Pizza", function() {
+  it("creates a new pizza", function () {
+    var testPizza = new Pizza("small", "1", "1");
+    expect(testPizza.pizzaSize).to.equal("small");
+    expect(testPizza.extraToppings).to.equal("1");
+    // expect(testPizza.howMany).to.equal("1");
+  });
+
   it("receives pizza size and returns price of pizza", function() {
     var testPizza = new Pizza("small", false);
     expect(testPizza.pizzaPrice()).to.equal("$8.50");
@@ -8,4 +15,9 @@ describe("Pizza", function() {
     var testPizza = new Pizza("medium", true);
     expect(testPizza.pizzaPrice()).to.equal("$12.00");
   });
+
+  // it("receives pizza quantity and returns price of pizza", function() {
+  //   var testPizza = new Pizza("large", true);
+  //   expect(testPizza.pizzaPrice()).to.equal("$28.00");
+  // });
 });
